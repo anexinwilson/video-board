764a2e9c4733fe5ca26980a8e519303b7257eaeb
anexinwilson <anexin777@gmail.com>
2025-09-08 19:28:04 -0700

    add api endpoint for uploading file to s3

---
+++ backend/src/model/videoSchema.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IVideo extends Document {
+  title?: string;
+  description?: string;
+  uploadeBy: mongoose.Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+  key: string;
+  path: string;
+  isPrivate: boolean;
+  thumbNail?: string;
+}
+const videoSchema: Schema = new Schema(
+  {
+    title: { type: String, default: "defualt title", required: true },
+    description: { type: String, default: "Default description" },
+    key: { type: String, required: true },
+    path: { type: String, required: true },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isPrivate: { type: Boolean, default: false },
+    thumbNail: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/005/048/106/small_2x/black-and-yellow-grunge-modern-thumbnail-background-free-vector.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+const Video: Model<IVideo> = mongoose.model<IVideo>("Video", videoSchema);
+export default Video;
--- backend/src/route/index.ts
+++ backend/src/route/index.ts
@@ -1,13 +1,18 @@
-import express from "express"
-import authRoute from "./authRoute"
+import express from "express";
+import authRoute from "./authRoute";
 import passport from "passport";
-import userRoute from "./userRoute"
-
+import userRoute from "./userRoute";
+import awsRoute from "./awsFileRoute";
 
 const router = express.Router();
 
-router.use('/auth',authRoute)
-router.use("/user/",
-passport.authenticate('jwt',{session:false}),userRoute)
+router.use("/auth", authRoute);
+router.use(
+  "/user/",
+  passport.authenticate("jwt", { session: false }),
+  userRoute
+);
+router.use("/aws", passport.authenticate("jwt", { session: false }), awsRoute);
 
-export default router
+
+export default router;
